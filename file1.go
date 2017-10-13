40265fcfc9540719fbe9fbf368f20f9e8795f3cd
rvRamakrishnan <ramakrishnan.vee@gmail.com>
2017-10-13 22:33:20 +0530

    Adding changes to meet Rel2

--- file1.go
+++ file1.go
@@ -4,4 +4,5 @@ import "fmt"
 func main() {
       fmt.Println("Code from remote repository -> https://github.com/rvRamakrishnan/golang")
       fmt.Println("After cloning, made some changes to code to be pushed to remote repository")
+      fmt.Println("Created Rel2 Branch. All further changes to code will be commited to Rel2 Branch")
 }
