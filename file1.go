b0334ec642b488a30b0963c1cb0c0710a60a2545
rvRamakrishnan <ramakrishnan.vee@gmail.com>
2017-10-13 22:11:37 +0530

    modified file1.go to include changes made by rvRamakrishnan to meet Rel1 Goals

--- file1.go
+++ file1.go
@@ -3,4 +3,5 @@ package main
 import "fmt"
 func main() {
       fmt.Println("Code from remote repository -> https://github.com/rvRamakrishnan/golang")
+      fmt.Println("After cloning, made some changes to code to be pushed to remote repository")
 }
